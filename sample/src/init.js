cd9eb11994fda264867d658555aeaecdb79e2701
Christian Paquin <cpaquin@microsoft.com>
2022-04-27 16:28:54 -0400

    Test page to test QR file upload

--- sample/src/init.js
+++ sample/src/init.js
@@ -1,4 +1,6 @@
 // copies js file from the node_modules folder for use by the html pages in ./public
 var fs = require('fs');
 fs.copyFileSync('node_modules/jsqr/dist/jsQR.js', 'public/jsQR.js');
+fs.copyFileSync('node_modules/png-js/png.js', 'public/png.js');
+fs.copyFileSync('node_modules/png-js/zlib.js', 'public/zlib.js');
 console.log("Copied QR-scanner scripts to ./public");
